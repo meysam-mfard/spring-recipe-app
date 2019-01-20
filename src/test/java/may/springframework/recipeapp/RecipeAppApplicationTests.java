6182f50b599d5b3fa2ef0726135c84f7a30c94f0
meysam-mfard <mmohagheghi@gmail.com>
2019-01-20 15:03:24 +0100

    Initialization.

---
+++ src/test/java/may/springframework/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,17 @@
+package may.springframework.recipeapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecipeAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
