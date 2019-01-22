aa8cc700de9fb3437474eb1e61bb6265c7069206
meysam-mfard <mmohagheghi@gmail.com>
2019-01-22 11:08:12 +0100

    Added Spring Data JPA query methods.

--- src/main/java/may/springframework/recipeapp/controllers/IndexController.java
+++ src/main/java/may/springframework/recipeapp/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package may.springframework.recipeapp.controllers;
 
+import may.springframework.recipeapp.Repositories.CategoryRepository;
+import may.springframework.recipeapp.Repositories.UnitOfMeasureRepository;
+import may.springframework.recipeapp.model.Category;
+import may.springframework.recipeapp.model.UnitOfMeasure;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id     is: " + categoryOptional.get().getId());
+        System.out.println("Unit of measure Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
