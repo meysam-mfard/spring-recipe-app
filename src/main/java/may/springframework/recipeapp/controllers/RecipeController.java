3d290728067f03031d1af416e6f13d0c80b7c1cf
meysam-mfard <mmohagheghi@gmail.com>
2019-04-07 08:29:43 +0200

    Create new recipe. form and controller

--- src/main/java/may/springframework/recipeapp/controllers/RecipeController.java
+++ src/main/java/may/springframework/recipeapp/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package may.springframework.recipeapp.controllers;
 
+import may.springframework.recipeapp.DTO.RecipeDto;
 import may.springframework.recipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +23,19 @@ public class RecipeController {
         model.addAttribute("recipe", recipeService.findById(new Long(id)));
 
         return "recipe/show";
+    }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeDto());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeDto recipeDto) {
+        RecipeDto saveRecipe = recipeService.saveRecipeDto(recipeDto);
+
+        return "redirect:/recipe/show/" + saveRecipe.getId();
     }
 }
